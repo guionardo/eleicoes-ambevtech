6f3594757509f79f5eb8fc926b4d6b8a16eb4063
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2023-03-21 09:20:05 -0300

    First commit

---
+++ Counter/Program.cs
@@ -0,0 +1,10 @@
+using Counter;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
---
+++ Dominio/Models/Voto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Models
+{
+    public record Voto(int IdEleicao, int idEleitor, int idCandidato);
+
+}
