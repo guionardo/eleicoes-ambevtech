1223dd00edad69e5be5a4157ddf8c025de149544
Guionardo Furlan <guionardo@gmail.com>
2023-03-31 14:44:10 -0300

    Adiciona classe de validações

---
+++ SharedResources/Validacoes/ModelValidations.cs
@@ -0,0 +1,28 @@
+﻿namespace SharedResources.Validacoes
+{
+    public static class ModelValidations
+    {
+        public static void ThrowForInvalidValue(bool condition, string message)
+        {
+            if (!condition)
+                throw new ArgumentException(message);
+        }
+
+        public static void ThrowForEmptyString(string value, string argumentName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException($"{argumentName} deve ser uma string não vazia");
+        }
+
+        public static void ThrowForNotPositiveNumber(int value, string argumentName)
+        {
+            if (value <= 0)
+                throw new ArgumentException($"{argumentName} deve ser um número positivo");
+        }
+
+        public static void ThrowForNegativeNumber(int value, string argumentName)
+        {
+            if (value < 0) throw new ArgumentException($"{argumentName} não pode ser um número negativo");
+        }
+    }
+}
