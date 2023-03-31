df96d354562e3593790609e585b47eac6ad93649
Guionardo Furlan <guionardo@gmail.com>
2023-03-31 15:51:50 -0300

    Adiciona gerador de dados para o provider

---
+++ Provider/Fakes/FakerEntities.cs
@@ -0,0 +1,45 @@
+﻿using Dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provider.Fakes
+{
+    public static class FakerEntities
+    {
+        public static List<string> Nomes(int quantidade)
+        {
+            var resultado = new List<string>();
+            for (var i = 0; i < quantidade; i++)
+            {
+                resultado.Add(Faker.Name.FullName());
+            }
+            return resultado;
+        }
+
+
+        public static List<Candidato> Candidatos(int quantidade)
+        {
+            var candidatos = new List<Candidato>();
+            for (var id = 1; id <= quantidade; id++)
+            {
+                candidatos.Add(new Candidato(id, Faker.Name.FullName()));
+
+            }
+            return candidatos;
+        }
+
+        public static List<Eleitor> Eleitores(int quantidade)
+        {
+            var eleitores = new List<Eleitor>();
+            for (var id = 1; id <= quantidade; id++)
+            {
+                eleitores.Add(new Eleitor(id, Faker.Name.FullName()));
+            }
+            return eleitores;
+
+        }
+    }
+}
--- Provider/Program.cs
+++ Provider/Program.cs
@@ -1,7 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Dominio.Models;
+using Provider.Fakes;
+
 Console.WriteLine("Provider!");
+
+
 // A função deste programa é simular a coleta de votos e enviar para a fila de processamento
 
 // 1. Início da votação
 // 2. Coleta de votos
 // 3. Fim da votação
+
+var candidatos = FakerEntities.Candidatos(5);
+var eleitores = FakerEntities.Eleitores(1000);
+
+var votacao = new Votacao(1, "Eleição para head da torre de vendas", candidatos, eleitores);
