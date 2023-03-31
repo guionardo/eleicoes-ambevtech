cbef1dd9faee4c62616b3929f8ba40b3ab41149c
Guionardo Furlan <guionardo@gmail.com>
2023-03-31 14:50:37 -0300

    Adiciona validação de IEnumerable vazio

--- SharedResources/Validacoes/ModelValidations.cs
+++ SharedResources/Validacoes/ModelValidations.cs
@@ -24,5 +24,12 @@
         {
             if (value < 0) throw new ArgumentException($"{argumentName} não pode ser um número negativo");
         }
+
+        public static void ThrowForNullOrEmptyEnumerable<T>(IEnumerable<T> enumerable,string argumentName)
+        {
+            if (enumerable == null || !enumerable.Any())
+                throw new ArgumentException($"{argumentName} não pode ser vazio");
+
+        }
     }
 }
