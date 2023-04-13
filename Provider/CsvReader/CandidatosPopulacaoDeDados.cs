d3b80e7dc45b666165295e906b7ab6d700ca9769
Daiana Weiss <daiana.weiss@ambevtech.com.br>
2023-04-13 16:02:27 -0300

    Iniciando a leitura dos candidatos a partir do csv

---
+++ Provider/CsvReader/CandidatosPopulacaoDeDados.cs
@@ -0,0 +1,25 @@
+﻿namespace Provider.CsvReader;
+
+/// <summary>
+/// Dados de população de candidatos
+/// Disponível em https://catalog.data.gov/dataset/electric-vehicle-population-data
+/// </summary>
+public record CandidatosPopulacaoDeDados
+{
+    public const string URL = @"https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD";
+
+    /// <summary>
+    /// Id do candidato
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Nome do candidato
+    /// </summary>
+    public string Nome { get; set; } = default!;
+
+    public override string ToString()
+    {
+        return $"Candidato: {Id} - {Nome}";
+    }
+}
